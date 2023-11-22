ad5c84be9f648c021c515fe9fd00376efe273604
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-11-22 12:00:11 +0530

    Create Creating Node.java

---
+++ Linked List/Creating Node.java
@@ -0,0 +1,11 @@
+class Node{
+        int data;//Holds the element value
+        Node next;//It holds the address of next node. In java it holds reference of the node so its Data type is of Class Node.
+
+        public  Node(int data) {
+             this.data=data;
+             this.next=null;
+        }
+
+}
+ 
