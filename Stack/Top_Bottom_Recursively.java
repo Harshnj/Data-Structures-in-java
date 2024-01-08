04d5a867b8d022e573f6aee2ba472c247912f9a2
Harshnj <95063086+Harshnj@users.noreply.github.com>
2024-01-08 21:44:50 +0530

    Create Top_Bottom_Recursively.java

---
+++ Stack/Top_Bottom_Recursively.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class display_Stack_Recursively {
+    static void print_Reverse_Recursive(Stack st){
+        int n=st.size();
+         if(n>0){
+             System.out.println(st.pop());
+             print_Reverse_Recursive(st);
+
+         }
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st= new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        print_Reverse_Recursive(st);
+
+    }
+
+}
